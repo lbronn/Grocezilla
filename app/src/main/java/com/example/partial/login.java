3e656ecaac0b3728f4bda82d74090f0bc95ebed1
Le Bronn <lebronn.samson@cit.edu>
2023-05-10 01:05:22 +0800

    Remember Me checbox is functional (needs a logout button to be fully implemented)

--- app/src/main/java/com/example/partial/login.java
+++ app/src/main/java/com/example/partial/login.java
@@ -4,10 +4,13 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -23,6 +26,7 @@ public class login extends AppCompatActivity {
     Button login;
     EditText email;
     EditText password;
+    CheckBox remember;
     FirebaseAuth mAuth;
 
 //    @Override
@@ -45,7 +49,18 @@ public class login extends AppCompatActivity {
         email = findViewById(R.id.editEmail);
         password = findViewById(R.id.editPass);
         login = findViewById(R.id.btnLogin);
+        remember = findViewById(R.id.loginRemember);
         createAccount = findViewById(R.id.createacc);
+
+        SharedPreferences preferences = getSharedPreferences("checkbox", MODE_PRIVATE);
+        String checkbox  = preferences.getString("remember", "");
+        if(checkbox.equals("true")) {
+            Intent i = new Intent(login.this, home.class);
+            startActivity(i);
+            finish();
+        } else if(checkbox.equals("false")) {
+            Toast.makeText(login.this, "Please Sign In.", Toast.LENGTH_SHORT).show();
+        }
 
         createAccount.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -91,6 +106,23 @@ public class login extends AppCompatActivity {
             }
         });
 
-
+        remember.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(compoundButton.isChecked()) {
+                    SharedPreferences preferences = getSharedPreferences("checkbox", MODE_PRIVATE);
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putString("remember", "true");
+                    editor.apply();
+//                    Toast.makeText(login.this, "Email and Password is Remembered.", Toast.LENGTH_SHORT).show();
+                } else if(!compoundButton.isChecked()) {
+                    SharedPreferences preferences = getSharedPreferences("checkbox", MODE_PRIVATE);
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putString("remember", "false");
+                    editor.apply();
+//                    Toast.makeText(login.this, "Email and Password is not Remembered.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
