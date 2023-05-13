cca9066a399ceb17c1bcb0e6ea0dbcb27aae04e4
Le Bronn <lebronn.samson@cit.edu>
2023-05-13 14:50:53 +0800

    Added functionality to Chat Button in Home.

--- app/src/main/java/com/example/partial/home.java
+++ app/src/main/java/com/example/partial/home.java
@@ -2,13 +2,39 @@ package com.example.partial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class home extends AppCompatActivity {
+    ImageView iChat;
+    TextView tChat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        iChat = findViewById(R.id.chatic);
+        tChat = findViewById(R.id.chattxt);
+
+        iChat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), message.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+        tChat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(home.this, message.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/partial/message.java
+++ app/src/main/java/com/example/partial/message.java
@@ -2,13 +2,27 @@ package com.example.partial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class message extends AppCompatActivity {
+    View chatToMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
+        chatToMenu = findViewById(R.id.chattomenu);
+
+        chatToMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(message.this, home.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
     }
 }
