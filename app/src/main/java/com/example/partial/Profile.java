546d63de508267f7e4d7a534ddc9718c830374f9
Le Bronn <lebronn.samson@cit.edu>
2023-05-25 00:17:33 +0800

    Beautified the UI for the alert dialogs, task, profile, and message (Message still needs modification).

--- app/src/main/java/com/example/partial/Profile.java
+++ app/src/main/java/com/example/partial/Profile.java
@@ -6,16 +6,27 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class Profile extends AppCompatActivity {
     TextView logout;
-
+    ImageButton backHome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         logout = findViewById(R.id.logout);
+        backHome = findViewById(R.id.backtohome);
+
+        backHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Profile.this, home.class);
+                startActivity(i);
+                finish();
+            }
+        });
 
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
